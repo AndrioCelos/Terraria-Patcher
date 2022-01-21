93d5fe118fed65272f3359871498670aa6749af6
Andrio Celos <AndrioCelos@users.noreply.github.com>
2022-01-21 16:42:03 +1100

    Add Remove Steam Achievements

---
+++ TerrariaPatcher/PatchSets/RemoveSteamAchievements.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+
+using System;
+
+using Terraria.Social;
+
+namespace TerrariaPatcher.PatchSets;
+
+internal class RemoveSteamAchievements : PatchSet {
+	public override string Name => "Remove Steam Achievements";
+	public override Version Version => new(1, 0);
+	public override string Description => "Disables integration of Steam achivements.";
+
+	internal class RemoveSteamAchievementsPatch : PrefixPatch {
+		public override PatchTarget TargetMethod => PatchTarget.Create(typeof(SocialAPI), "LoadSteam");
+		public static void Postfix() => SocialAPI.Achievements = null;
+	}
+}
