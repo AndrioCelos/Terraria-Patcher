0f15b557e801c2bf133a0581fff17691a6c5b869
Andrio Celos <AndrioCelos@users.noreply.github.com>
2022-01-27 12:58:29 +1100

    Add Metal Detector Adjustments

---
+++ TerrariaPatcher/PatchSets/MetalDetectorMod.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+using System;
+
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.UI;
+using Terraria;
+
+namespace TerrariaPatcher.PatchSets;
+
+internal class MetalDetectorMod : PatchSet {
+	public override string Name => "Metal Detector Adjustments";
+	public override Version Version => new(1, 0);
+	public override string Description => "Changes the Metal Detector to prioritise life crystals just below chests.";
+
+	public class MetalDetectorPatch : PrefixPatch {
+		public override PatchTarget TargetMethod => PatchTarget.Create(typeof(Main), "SetTileValue");
+
+		public static void Postfix()
+			=> Main.tileOreFinderPriority[TileID.Heart] = (short) (Main.tileOreFinderPriority[TileID.Containers] - 1);
+	}
+}
