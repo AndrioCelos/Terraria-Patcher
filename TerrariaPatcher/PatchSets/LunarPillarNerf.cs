0ad4797f1b119f14b5f8f56eb5659b382efbaa87
Andrio Celos <AndrioCelos@users.noreply.github.com>
2022-02-24 10:41:15 +1100

    Add Lunar Pillar Nerf

---
+++ TerrariaPatcher/PatchSets/LunarPillarNerf.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+using System;
+
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.UI;
+using Terraria;
+
+namespace TerrariaPatcher.PatchSets;
+
+internal class LunarPillarNerf : PatchSet {
+	public override string Name => "Lunar Pillar Nerf";
+	public override Version Version => new(1, 0);
+	public override string Description => "Reduces the number of kills needed to break the lunar pillars' shields in expert mode to the same as in normal mode. This works only in single player.";
+
+	public class LunarPillarNerfPatch : PrefixPatch {
+		public override PatchTarget TargetMethod => PatchTarget.Create(typeof(NPC), $"get_{nameof(NPC.ShieldStrengthTowerMax)}");
+
+		public static bool Prefix(out int __result) {
+			__result = NPC.LunarShieldPowerNormal;
+			return Main.netMode == 1;  // Will fall through to the original method in a multiplayer client.
+		}
+	}
+}
