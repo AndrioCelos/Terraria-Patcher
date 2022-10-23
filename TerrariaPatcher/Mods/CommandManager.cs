db2a50f1a0f8a645079339bbeb7e7bb155cc1623
Andrio Celos <AndrioCelos@users.noreply.github.com>
2022-10-24 10:33:28 +1100

    Command Manager: Suppress hotkeys while typing in the duplication menu

--- TerrariaPatcher/Mods/CommandManager.cs
+++ TerrariaPatcher/Mods/CommandManager.cs
@@ -189,7 +189,8 @@ internal static class CommandManager {
 	}
 
 	public static void HandleInput(IEnumerable<Keys> pressedKeys) {
-		if (Main.drawingPlayerChat || Main.editSign || Main.editChest || Main.blockInput) return;
+		if (Main.drawingPlayerChat || Main.editSign || Main.editChest || Main.blockInput || Main.CurrentInputTextTakerOverride != null)
+			return;
 
 		currentKeys.Clear();
 		currentModifiers = 0;
